046b727ca12144364cadcffcc2222adc62b2cba7
iDiontep <s20diontep@gmail.com>
2024-03-08 09:30:14 +0300

    update 5 week conspects

--- tau/labs/lab2/task1a/plotLocus.m
+++ tau/labs/lab2/task1a/plotLocus.m
@@ -7,7 +7,7 @@ tspan = [0, tmax]; % отрезок времени, на котором моде
 for i = x1(1, :)
     for j = transpose(x2(:, 1))
         x0 = [i; j];  
-        [~, z, ~, ~, ~] = ode23t(dx, tspan, x0, odeset('RelTol',1e-5,'Events', event_fnc)); % задаем параметры солвера
+        [~, z, ~, ~, ~] = ode23t(dx, tspan, x0, odeset('RelTol',1e-3,'Events', event_fnc)); % задаем параметры солвера
         plot(z(:, 1), z(:, 2), 'c'); % строим найденное решение
     end
 end
--- tau/labs/lab2/task1a/run.m
+++ tau/labs/lab2/task1a/run.m
@@ -2,15 +2,15 @@ clc;
 clear all;
 close all;
 
-XMAX = 5;                   % размер сетки рисования
-STEP = 0.5;                 % шаг сетки
-TMAX = 10;                  % время моделирования
+XMAX = 3;                   % размер сетки рисования
+STEP = 0.01;                 % шаг сетки
+TMAX = 3;                  % время моделирования
 
 [x1, x2] = meshgrid(-XMAX: STEP: XMAX);
 
 tic; % остановка  и запуск секундомера
 dx = @(t, x) moi_variant(t, x);
-event_out_of_bounds = @(t, z) outOfBounds(t, z, XMAX+0.01);
+event_out_of_bounds = @(t, z) outOfBounds(t, z, XMAX+0.005);
 plotLocus(x1, x2, @moi_variant, event_out_of_bounds, TMAX); % строим фазовые траектории
 plotQuiver(x1, x2, @moi_variant); % строим стрелочки
 
